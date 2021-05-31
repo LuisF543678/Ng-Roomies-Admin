fdb07af4c74bfc835135edcfe1721be10512dd9a
Luisf543678 <luisf543678@gmail.com>
2021-05-31 18:00:26 -0500

    starting with the branch

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -16,16 +16,16 @@ const routes: Routes = [
     path: 'menu', component: MenuComponent
   },
   {
-    path: 'estadisticas', component: EstadisticasComponent // no
+    path: 'estadisticas', component: EstadisticasComponent 
   },
   {
-    path: 'gestion-arrendados', component: GestionarrendadosComponent // si
+    path: 'gestion-arrendados', component: GestionarrendadosComponent 
   },
   {
-    path: 'peticiones-arrendamientos', component: PeticionesarrendamientoComponent // si
+    path: 'peticiones-arrendamientos', component: PeticionesarrendamientoComponent 
   },
   {
-    path: 'lista-arrendados', component: ListaarrendadosComponent // si
+    path: 'lista-arrendados', component: ListaarrendadosComponent 
   },
   {
     path: 'gestion-reportes', component: GestionreportesComponent
