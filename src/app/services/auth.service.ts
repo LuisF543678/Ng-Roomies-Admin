bd440ac1e8311ffb65b263ee0d9ab33bb3912323
MarxBlite <33582738+MarxBlite@users.noreply.github.com>
2021-06-19 20:26:19 -0500

    Se añadió firebase al proyecto y se configuraron las credenciales de uso. (#3)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,21 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
-// Crear un interceptor
-  protected authUser(user: any) {
-    this.getResponse(this.httpClient.post('', user));
-  }
-
-  private getResponse(http: any) {
-    return new Promise((resolve: any, reject: any)=> {
-      http.subscribe((res: any) => {
-        resolve(res)
-      },
-      (error: any) => {
-        reject(error);
-        console.log(error);
-      });
-    })
-   }
+  constructor() { }
 }
