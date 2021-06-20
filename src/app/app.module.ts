ced424df5301552d346e785322fc2b9641bc9a97
unknown <olivergarciaw7.0@gmail.com>
2021-06-19 19:30:57 -0500

    Se añadió firebase al proyecto y se configuraron las credenciales de uso.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,18 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { AngularFireModule } from '@angular/fire';
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDlaea9L46r--_5ivWSn--ShFfQkrbu9cc",
+  authDomain: "teocalli-af7b1.firebaseapp.com",
+  databaseURL: "https://teocalli-af7b1-default-rtdb.firebaseio.com",
+  projectId: "teocalli-af7b1",
+  storageBucket: "teocalli-af7b1.appspot.com",
+  messagingSenderId: "331630436932",
+  appId: "1:331630436932:web:8e5672360350cb30779211",
+  measurementId: "G-DH9HB6K6W4"
+};
 
 @NgModule({
   declarations: [
@@ -33,7 +43,8 @@ import {MatIconModule} from '@angular/material/icon';
     HttpClientModule,
     MainRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
