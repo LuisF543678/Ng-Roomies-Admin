c1e3e478a29a767cd5a13a110a0210898e1c3e18
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-30 11:30:58 -0500

    modify: Creating User and UserRoles interfaces.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,27 +1,15 @@
-export class User {
-    constructor(
-        public ID: number,
-        public UUID: string,
-        public user_name: string,
-        public name: string,
-        public father_surname: string,
-        public mother_surname: string,
-        public ProfileImage: string,
-        public user_role_id: number,
-        public gender: string,
-        public UserRole: UserRole
-    ) {}
-
-    static createVoid(): User {
-        const role = new UserRole(0,'','');
-        return new User(0, '', '', '', '','','',0,'',role);
-    }
+export interface User {
+    username: string;
+    firstName: string;
+    fatherSurname: string;
+    motherSurname: string;
+    profileImage: string;
+    gender: string;
+    role: UserRole;
+    id?: string;
 }
 
-export class UserRole {
-    constructor(
-        public ID: number,
-        public Name: string,
-        public SlugName: string
-    ) {}
+export interface UserRole {
+    name: string;
+    slugName: string;
 }
