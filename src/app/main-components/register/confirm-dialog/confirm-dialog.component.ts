c7f00e1af58631ebf053c4be71dae31e18ea9fa8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-07-31 14:57:35 -0500

    modify: ConfirmDialogComponent structure finished

--- src/app/main-components/register/confirm-dialog/confirm-dialog.component.ts
+++ src/app/main-components/register/confirm-dialog/confirm-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from 'src/app/models/vo/dialogData';
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onClose(): void {
+    this.dialogRef.close();
+  }
 }
