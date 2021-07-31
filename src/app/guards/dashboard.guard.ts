fa21c2bec2edf88ed3ba19598dc58ce525423875
Luisf543678 <luisf543678@gmail.com>
2021-07-31 10:49:50 -0500

    change 1

--- src/app/guards/dashboard.guard.ts
+++ src/app/guards/dashboard.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-
+/* import { Observable } from 'rxjs';
+ */
 @Injectable({
   providedIn: 'root'
 })
