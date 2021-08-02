599516c2b98e113ab7538132c4fb8bd3ebde3808
Luisf543678 <luisf543678@gmail.com>
2021-08-02 12:20:52 -0500

    test123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [{
   path: 'admin',
 
   loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-  canActivate:[DashboardGuard]
+  /* canActivate:[DashboardGuard] */
 },
 // Important the route must be here
 { path: '404', component: NotfoundComponent },
