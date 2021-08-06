3a24e2628706386bf745720e2c6424b371d052e6
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-06 14:30:05 -0500

    modify: Adding createAccommodation method in AccommodationService

--- src/app/services/accommodation.service.ts
+++ src/app/services/accommodation.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Accommodation } from '../models/accomodation';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccommodationService {
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) { }
+
+  public createAccommodation(accommodation: Accommodation, manager: User): void {
+    accommodation.manager = manager;
+    this.database.list<Accommodation>('alojamientos').push(accommodation);
+  }
 }
