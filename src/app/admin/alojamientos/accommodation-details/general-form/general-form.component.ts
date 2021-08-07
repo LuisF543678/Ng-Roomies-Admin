9f7c07de7cc0093a186e42b4f26aa19db5f09ab5
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-07 18:24:01 -0500

    modify: Implementing General form component in Accommodation details page

--- src/app/admin/alojamientos/accommodation-details/general-form/general-form.component.ts
+++ src/app/admin/alojamientos/accommodation-details/general-form/general-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Accommodation } from 'src/app/models/accomodation';
 
 @Component({
   selector: 'app-general-form',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-form.component.css']
 })
 export class GeneralFormComponent implements OnInit {
+  @Input()
+  accommodation: Accommodation;
+  
+  form: FormGroup;
 
-  constructor() { }
-
+  constructor(
+    private builder: FormBuilder,
+  ) {
+  }
+  
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.builder.group({
+      name: [this.accommodation.name, [Validators.required]],
+      price: [this.accommodation.price, [Validators.required]],
+      rooms: [this.accommodation.rooms, [Validators.required]],
+      startDay: [this.accommodation.schedule.startDay, [Validators.required]],
+      endDay: [this.accommodation.schedule.endDay, [Validators.required]],
+      startHour: [this.accommodation.schedule.startHour, [Validators.required]],
+      endHour: [this.accommodation.schedule.endHour, [Validators.required]]   
+    });
+    this.form.disable();
   }
 
 }
