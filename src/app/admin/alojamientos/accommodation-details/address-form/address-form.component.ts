e0442299c461a3653e8fa5314bcd6432e92f94b4
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-07 19:05:18 -0500

    modify: Accommodation details feature finished.

--- src/app/admin/alojamientos/accommodation-details/address-form/address-form.component.ts
+++ src/app/admin/alojamientos/accommodation-details/address-form/address-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Accommodation } from 'src/app/models/accomodation';
 
 @Component({
   selector: 'app-address-form',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-form.component.css']
 })
 export class AddressFormComponent implements OnInit {
+  @Input()
+  accommodation: Accommodation;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private builder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.builder.group({
+      street: [this.accommodation.location.street, [Validators.required]],
+      outdoorNumber: [this.accommodation.location.outdoorNumber, [Validators.required]],
+      district: [this.accommodation.location.district, [Validators.required]],
+      zipCode: [this.accommodation.location.zipCode, [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      city: [this.accommodation.location.city, [Validators.required]],
+      state: [this.accommodation.location.state, [Validators.required]]
+    });
+
+    this.form.disable();
   }
 
 }
