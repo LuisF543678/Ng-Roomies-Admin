91d27f77e55a180f7cb7dcc16badd1cea0a65abb
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-07 23:29:48 -0500

    modify: Edit firstPhoto feature finished.

--- src/app/admin/alojamientos/accommodation-details/photo-dialog/photo-dialog.component.ts
+++ src/app/admin/alojamientos/accommodation-details/photo-dialog/photo-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AccommodationService } from 'src/app/services/accommodation.service';
 
 @Component({
   selector: 'app-photo-dialog',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoDialogComponent implements OnInit {
 
-  constructor() { }
+  preview: string | ArrayBuffer;
+  form: FormGroup;
+  url: string;
+  file: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<PhotoDialogComponent>,
+    private accommodationService: AccommodationService,
+    private builder: FormBuilder
+    ) {
+      this.initForm();
+    }
 
   ngOnInit(): void {
   }
 
+  initForm(): void {
+    this.form = this.builder.group({
+      photo: ['', [Validators.required]],
+    });
+  }
+
+  onChange(event: any): void {
+    this.file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.file);
+    reader.onload = () => {
+      this.preview = reader.result;
+    }
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  async uploadPhoto(): Promise<void> {
+    this.url = await this.accommodationService.addFirstPhoto(this.file);
+    this.dialogRef.close(this.url);
+  }
 }
