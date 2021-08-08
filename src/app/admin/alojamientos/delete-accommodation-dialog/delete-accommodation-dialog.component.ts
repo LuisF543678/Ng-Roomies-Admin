77444728c3746971d233d7c8d5afdd639f310dfd
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-08 00:04:19 -0500

    add: DeleteAccommodationDialog component created.

---
+++ src/app/admin/alojamientos/delete-accommodation-dialog/delete-accommodation-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-accommodation-dialog',
+  templateUrl: './delete-accommodation-dialog.component.html',
+  styleUrls: ['./delete-accommodation-dialog.component.css']
+})
+export class DeleteAccommodationDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
