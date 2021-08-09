ae58a89132357826935f149de7fedd73a3377eb9
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-09 17:02:39 -0500

    modify: Implementing enable and disable edit in form

--- src/app/admin/components/profile-form/profile-form.component.ts
+++ src/app/admin/components/profile-form/profile-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile-form',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-form.component.css']
 })
 export class ProfileFormComponent implements OnInit {
+  @Input()
+  user: User;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private builder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.form = this.builder.group({
+      firstName: [this.user.firstName, [Validators.required]],
+      fatherSurname: [this.user.fatherSurname, [Validators.required]],
+      motherSurname: [this.user.motherSurname, [Validators.required]],
+      birthDate: [this.user.birthDate, [Validators.required]],
+      gender: [this.user.gender, [Validators.required]],
+    });
+
+    this.form.disable();
+  }
+
+  enableForm(): void {
+    this.form.enable();
+  }
+
+  disableForm(): void {
+    this.form.disable();
+  }
 }
