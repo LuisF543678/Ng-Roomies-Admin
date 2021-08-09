6d6364101429e2d29dc128e61f45b972e9024ede
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-09 17:21:41 -0500

    modify: Update profile information feature finished

--- src/app/admin/components/profile-form/profile-form.component.ts
+++ src/app/admin/components/profile-form/profile-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile-form',
@@ -15,6 +16,7 @@ export class ProfileFormComponent implements OnInit {
 
   constructor(
     private builder: FormBuilder,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
@@ -38,6 +40,20 @@ export class ProfileFormComponent implements OnInit {
   }
 
   disableForm(): void {
+    this.form.disable();
+  }
+
+  updateProfileData(): void {
+    if (this.form.valid) {
+      this.user.firstName = this.form.get('firstName').value;
+      this.user.fatherSurname = this.form.get('fatherSurname').value;
+      this.user.motherSurname = this.form.get('motherSurname').value;
+      this.user.gender = this.form.get('gender').value;
+      this.user.birthDate = this.form.get('birthDate').value;
+
+      localStorage.setItem('user', JSON.stringify(this.user));
+      this.authService.updateUser(this.user);
+    }
     this.form.disable();
   }
 }
