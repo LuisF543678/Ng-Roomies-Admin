dc89c35d998f5492d0050bcc7bb8c7e3df0084a2
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-09 19:15:12 -0500

    modify: Delete contact feature finished.

--- src/app/admin/components/contact/contact.component.ts
+++ src/app/admin/components/contact/contact.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Contact, User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { DeleteContactDialogComponent } from '../delete-contact-dialog/delete-contact-dialog.component';
 import { UpdateContactDialogComponent } from '../update-contact-dialog/update-contact-dialog.component';
 
 @Component({
@@ -53,4 +54,19 @@ export class ContactComponent implements OnInit, OnChanges {
     );
   }
 
+  showDeleteDialog(): void {
+    const dialogRef = this.matDialog.open(DeleteContactDialogComponent);
+
+    this.dialogSubscriber = dialogRef.afterClosed().subscribe(
+      (data: boolean) => {
+        if (data) {
+          const contactsFiltered = this.user.contacts.filter(e => (e.name != this.contact.name && e.value != this.contact.value));
+          this.user.contacts = contactsFiltered;
+          localStorage.setItem('user', JSON.stringify(this.user));
+          this.authService.updateUser(this.user);
+        }
+      },
+      console.error
+    );
+  }
 }
